eb981d290020c75ac58f25fbfffc39e7d5d9f81f
dubeyajit <ajitdubey@gmail.com>
2022-01-10 12:13:40 +0530

    Add files via upload

--- workdir/main.py
+++ workdir/main.py
@@ -1,19 +1,22 @@
 from flask import Flask
-from flask_restful import Api, Resource, reqparser
+from flask_restful import Api, Resource, reqparse, abort
 
 app = Flask(__name__)
 api = Api(app)
 
-video_put_args = reqparser.RequestParser()
-video_put_args.add_argument("Name", type=str, help="Name of the video")
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("Name", type=str, help="Name of the video is required", required=True)
 video_put_args.add_argument("Views", type=int, help="Views of the video")
 video_put_args.add_argument("Likes", type=int, help="Likes on the video")
 
-videos[] = {}
-
+videos = {}
 names = {"Ajit": {"Age":49, "Gender":"Male"},
          "Sheela": {"Age":49, "Gender":"Female"}
 }
+
+def abort_if_video_id_doesnt_exist(video_id):
+    if video_id not in videos:
+        abort(404, message="Could not find video")
 
 class HelloWorld(Resource):
     def get(self,name):
@@ -21,11 +24,13 @@ class HelloWorld(Resource):
 
 class Video(Resource):
     def get(self, video_id):
+        abort_if_video_id_doesnt_exist(video_id)
         return videos[video_id]
     
     def put(self, video_id):
         args = video_put_args.parse_args()
-        return {video_id: args}
+        videos[video_id] = args
+        return videos[video_id], 201
 
 
 api.add_resource(HelloWorld, "/helloworld/<string:name>/")
